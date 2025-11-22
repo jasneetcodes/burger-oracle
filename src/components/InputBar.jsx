4e6e9877764ba5d45834d4b0e1b4970a0ccb01f5
Jasneet Singh <100010929+jasneetcodes@users.noreply.github.com>
2025-11-22 14:58:19 -0700

    feat: implement InputBar with message state, and send button

--- src/components/InputBar.jsx
+++ src/components/InputBar.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
 
 export default function InputBar() {
+
+    const [message, setMessage] = useState("");
+    const [isSending, setIsSending] = useState(false);
+
+
+    async function handleSendMessage(){
+        const trimmedMessage = message.trim();
+
+        setIsSending(true);
+        // function to send Trimmedmessage
+        setIsSending(false);
+        setMessage("");
+    }
+
+    function handleEnterPress(e){
+        const trimmedMessage = message.trim();
+        if(e.key === "Enter" && !isSending){
+            // function to send Trimmedmessage
+        }
+    }
   return (
     <div>
-      <input type="text" placeholder="Type here..." />
+      <div>
+        <input type="text" placeholder="Send a message..." value={message} 
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyUp={(e) => handleEnterPress(e)}
+        />
+      </div>
+      <div>
+        <button
+       // function to send message onClick={}
+       disabled={isSending}
+        >Send</button>
+      </div>
     </div>
   );
 }
